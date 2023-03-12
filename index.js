fa2c0cd0f3137923f27491a221808a1a17d046e4
Kenneth <kenneth.ebue@gmail.com>
2023-03-12 14:34:00 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, giftType) {
+    const messages = [];
+  
+    names.forEach((name) => {
+      messages.push(`Thank you, ${name}, for the wonderful ${giftType} gift!`);
+    });
+  
+    return messages;
+  }
+  
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
